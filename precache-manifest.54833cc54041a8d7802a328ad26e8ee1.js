1b264ec9137585402282958d3c4367994d76987f
ceahmad <almansour.a.ahmad@gmail.com>
2019-02-20 16:06:00 +0300

    Updates

---
+++ precache-manifest.54833cc54041a8d7802a328ad26e8ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55698e1ac12b27216188",
+    "url": "/project-2/static/css/main.fdc7ce05.chunk.css"
+  },
+  {
+    "revision": "55698e1ac12b27216188",
+    "url": "/project-2/static/js/main.55698e1a.chunk.js"
+  },
+  {
+    "revision": "8204681cae27f55d9384",
+    "url": "/project-2/static/js/runtime~main.8204681c.js"
+  },
+  {
+    "revision": "a75237ea0ef595a14061",
+    "url": "/project-2/static/js/2.a75237ea.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9bcbc91d51659339588324fecaff9ce",
+    "url": "/project-2/index.html"
+  }
+];
